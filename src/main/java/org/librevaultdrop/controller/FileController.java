11caa85ffca1391daaeab0a4425b1efe9039c912
Abdurrahman-shaikh <abdurrahmanshaikh830@gmail.com>
2025-03-23 08:58:22 +0530

    setup-the-project-structure

---
+++ src/main/java/org/librevaultdrop/controller/FileController.java
@@ -0,0 +1,11 @@
+package org.librevaultdrop.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/vi/file")
+public class FileController {
+
+    
+}
